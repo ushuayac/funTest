d10547009a39e4af841db7aae2471da1237be5e2
Ushuaya C. <ushikid@protonmail.com>
2025-10-16 13:22:30 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # funTest
+
 a simple script to display battery diagnostics and open functionality testing in Safari during MacOS OOBE (setup screen)
---
+++ funtest.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+
+# Connect to wifi (doesn't always work for some reason)
+echo "Attempting to connect wifi, connect manually if unsuccessful!"
+networksetup -setairportnetwork en0 "Aaxl" "\][poiuy"
+echo
+
+# Grab and display Battery Cycle info
+battCycles="$(system_profiler SPPowerDataType | grep -e "Cycle Count")"
+battCycles="$(echo "$battCycles" | grep -o '[0-9]\+')"
+echo "BATTERY CYCLES: $battCycles"
+echo
+
+# Grab design/max capacity to calculate and display health
+designCap=$(ioreg -l | grep -Fw "DesignCapacity" | tail -1 | grep -o '[0-9]\+')
+maximumCap=$(ioreg -l | grep -Fw "MaxCapacity" | tail -1 | grep -o '[0-9]\+')
+echo "Current Max Capacity: $maximumCap"
+echo "Original Design Capacity: $designCap"
+echo
+
+batteryHealth=$(echo "scale=2; $maximumCap / $designCap" | bc | grep -o '[0-9]\+')
+
+echo "BATTERY HEALTH: $batteryHealth percent"
+echo "(Battery health is Maximum Cap divided by Design Cap)"
+echo
+
+# Open safari for functionality testing on retest.us
+open -a Safari "https://spinning.fish/" "https://testmyscreen.com/" "https://retest.us"
+if pgrep -q "Safari"; then
+        echo "Safari successfully started!"
+    else
+        # Next try opening by pointing to the .app in the Application directory
+        open -a "/Applications/Safari.app" "https://spinning.fish/" "https://testmyscreen.com/" "https://retest.us"
+                
+        if pgrep -q "Safari"; then
+            echo "Safari successfully started!"
+        else
+            echo "Can't find Safari window! Please start the application manually."
+            # Finally, fall back to opening Finder for a manual start
+            open "/Applications/"
+        fi
+    fi
